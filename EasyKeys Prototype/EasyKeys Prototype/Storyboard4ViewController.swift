8105aefe748499bf2316a6441f284a1593db9807
Ishaan Simran Tuli <156983843+ishaansimrantuli7@users.noreply.github.com>
2025-11-03 11:56:18 +0530

    files added.

---
+++ EasyKeys Prototype/EasyKeys Prototype/Storyboard4ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Storyboard4ViewController.swift
+//  EasyKeys Prototype
+//
+//  Created by Student on 03/11/25.
+//
+
+import UIKit
+
+class Storyboard4ViewController: UIViewController {
+
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        label.text = textfield.text
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
